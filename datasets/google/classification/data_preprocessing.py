54e703f1cae53d71569b61fca77b50de5a7360b1
Michele Milesi <michele.milesi@studio.unibo.it>
2022-09-28 09:53:39 +0200

    Ci siamo quasi

--- datasets/google/classification/data_preprocessing.py
+++ datasets/google/classification/data_preprocessing.py
@@ -3,9 +3,9 @@ import numpy as np
 
 train = pd.read_csv("./train.csv")
 train["rowtype"] = ["TRAIN" for i in range(train.shape[0])]
-val_indexes = np.random.choice(range(train.shape[0]), int(train.shape[0] * 0.1), replace = False)
+val_indices = np.random.choice(range(train.shape[0]), int(train.shape[0] * 0.1), replace = False)
 
-train.loc[val_indexes, "rowtype"] = ["VALIDATE" for i in range(int(train.shape[0] * 0.1))]
+train.loc[val_indices, "rowtype"] = ["VALIDATE" for i in range(int(train.shape[0] * 0.1))]
 
 test = pd.read_csv("./test.csv")
 test["rowtype"] = ["TEST" for i in range(test.shape[0])]
--- datasets/google/regression/data_processing.py
+++ datasets/google/regression/data_processing.py
@@ -3,14 +3,14 @@ import numpy as np
 
 train = pd.read_csv("./train.csv")
 train["rowtype"] = ["TRAIN" for i in range(train.shape[0])]
-val_indexes = np.random.choice(range(train.shape[0]), int(train.shape[0] * 0.2), replace = False)
+val_indices = np.random.choice(range(train.shape[0]), int(train.shape[0] * 0.2), replace = False)
 
-print(len(val_indexes[:int(train.shape[0] * 0.1) + 1]))
+print(len(val_indices[:int(train.shape[0] * 0.1) + 1]))
 print(len(["VALIDATE" for i in range(int(train.shape[0] * 0.1))]))
 
-train.loc[val_indexes[:int(train.shape[0] * 0.1) + 1], "rowtype"] = ["VALIDATE" for i in range(int(train.shape[0] * 0.1) + 1)]
+train.loc[val_indices[:int(train.shape[0] * 0.1) + 1], "rowtype"] = ["VALIDATE" for i in range(int(train.shape[0] * 0.1) + 1)]
 
-train.loc[val_indexes[int(train.shape[0] * 0.1) + 1:], "rowtype"] = ["TEST" for i in  range(int(train.shape[0] * 0.1) + 1, int(train.shape[0] * 0.2))]
+train.loc[val_indices[int(train.shape[0] * 0.1) + 1:], "rowtype"] = ["TEST" for i in  range(int(train.shape[0] * 0.1) + 1, int(train.shape[0] * 0.2))]
 
 train.to_csv("./regression.csv")
 
